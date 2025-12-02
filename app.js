8b62cad38f7621ae361a1b7dd9be397a3bbcbec4
Anbuselvan04 <adsanbuselvan@gmail.com>
2025-12-02 23:42:42 +0530

    Swagger Commit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 require("express-async-errors");
 const express = require("express");
 const app = express();
+const path = require('path')
 
 // Extra Security Packages
 
@@ -10,10 +11,11 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+
 //Swagger 
 const swaggerUI = require('swagger-ui-express')
 const YAML = require('yamljs')
-const swaggerDocument = YAML.load('swagger.yaml')
+const swaggerDocument = YAML.load(path.join(__dirname, 'swagger.yaml'))
 
 //connect DB
 const connectDB = require("./db/connect");
