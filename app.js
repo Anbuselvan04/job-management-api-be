9f6bcda9bea6c45332403d58d85bf76c11af4657
Anbuselvan04 <adsanbuselvan@gmail.com>
2025-12-02 23:48:58 +0530

    Swagger Commit

--- app.js
+++ app.js
@@ -49,6 +49,7 @@ app.get('/',(req,res)=>
     res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>')
 })
 
+
 app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerDocument))
 
 // routes
